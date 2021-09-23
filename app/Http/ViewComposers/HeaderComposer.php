87125a98c6df9f06e20ca8eee9ab20a71236136c
medericgb <medericjoel@gmail.com>
2021-09-23 11:13:53 +0000

    Ajout de HeaderComposer.php

--- app/Http/ViewComposers/HeaderComposer.php
+++ app/Http/ViewComposers/HeaderComposer.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\ViewComposers;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
 use Illuminate\View\View;
 
